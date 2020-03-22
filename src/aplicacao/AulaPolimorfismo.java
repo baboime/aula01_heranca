3e0ee2e535fdfce0cc353e4aa451f98ff2dfa497
Edney Baboim <ednney@gmail.com>
2020-03-22 18:58:19 -0300

    Atualização Aula Polimorfismo

---
+++ src/aplicacao/AulaPolimorfismo.java
@@ -0,0 +1,21 @@
+package aplicacao;
+
+import entidades.Conta;
+import entidades.ContaPoupanca;
+
+public class AulaPolimorfismo {
+
+	public static void main(String[] args) {
+		
+		Conta x = new Conta(1020, "Alex", 1000.0);
+		Conta y = new ContaPoupanca(1023, "Maria", 1000.0, 0.01);
+		
+		x.saque(50.0);
+		y.saque(50.0);
+		
+		System.out.println(x.getSaldo());
+		System.out.println(y.getSaldo());
+
+	}
+
+}
